298a5f5eab0c77f992c3e5292c6ee0961d17bcb8
Akongnwi Che Devert <akongnwi.devert@cm.maviance.com>
2019-03-14 23:15:26 +0100

    portfolio finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
-import profile from './assets/PROFILE 4X4.jpeg';
+import profile from '../assets/PROFILE 4X4.jpeg';
+import Title from './Title';
 class App extends Component {
     state = { displayBio: false };
 
@@ -13,12 +14,13 @@ class App extends Component {
             <div>
                 <img src={profile} alt='profile-picture' className="profile"/>
                 <h1>Hello!</h1>
-                <p>My name is David. I'm a software engineer</p>
+                <p>My name is David.</p>
+                    <Title />
                 <p>I'm always looking forward to working on meaningful projects</p>
                 {
                     this.state.displayBio ? (
                         <div>
-                            <p>Live in san francisco and code everyday.</p>
+                            <p>Live  in san francisco and code everyday.</p>
                             <p>My favorite language is Javascript, and I lthink React.js is awesome.</p>
                             <p>Besides coding, I love music and ramen!</p>
                             <button onClick={this.toggleDisplayBio}>
@@ -34,9 +36,9 @@ class App extends Component {
                     )
                 }
                 <hr/>
-                <Projects/>
+                <Projects />
                 <hr/>
-                <SocialProfiles/>
+                <SocialProfiles />
             </div>
         );
     }
